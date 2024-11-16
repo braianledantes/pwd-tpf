93307d11c3c4d48238cad156ca3a09c2cc980de2
lucianaromano <luciana.romano@fi.uncoma.edu.ar>
2024-11-16 02:27:36 -0300

    algo de los menusss

---
+++ vista/menus/menuCliente.php
@@ -0,0 +1,17 @@
+
+<ul class="hList">
+  <li>
+    <a href="#click" class="menu">
+      <h2 class="menu-title">ABM Datos</h2>
+      <ul class="menu-dropdown">
+        <li href="#">Modificar e-mail</li>
+        <li href="#">Modificar contraseña</li>
+      </ul>
+    </a>
+  </li>
+  <li>
+    <a href="#click" class="menu">
+      <h2 class="menu-title menu-title_2nd" href="#">Comprar</h2>   <!---LLEVAR AL CARRITO/PRODUCTOS --->
+    </a>
+  </li>
+</ul>  
