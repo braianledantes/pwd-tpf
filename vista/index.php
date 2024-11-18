d66192c1aabc5293a97b3ba7ef1bcf506307e469
ClaraMelaniePelozo <clara.pelozo@est.fi.uncoma.edu.ar>
2024-11-17 21:17:32 -0300

    arregle lo de los links descompuestos

--- vista/index.php
+++ vista/index.php
@@ -23,35 +23,51 @@ $session = new Sesion();
 <body>
     <?php include_once("./estructura/cabecera.php"); ?>
     <!-- Carrusel -->
-     <section style=" margin-top:-17px;">
-     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button id="tienda"type="button"><a href="./productos/index.php">Ver productos!</a></button>
+     <section style="margin-top:-17px;">
+        <div id="carouselExample" class="carousel" data-bs-ride="carousel">
+        <!-- Indicators -->
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+
+            <!-- Slides -->
+        <div class="carousel-inner ">
+            <div class="carousel-item active">
+                <div class="d-flex flex-column align-items-center justify-content-center" style="height: 500px; background-color: #f8f9fa;">
+                <img src="./assets/imagenes/anillo1.png" class=" w-100" alt="Anillo de alas plateado">
+                     <button class="btn btn-outline-dark" style="position: absolute; top:20%; left: 50%; transform: translate(-50%, -50%);z-index:-1">Ver productos</button>
+                </div>
             </div>
-        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="./assets/imagenes/anillo1.png" class=" w-100" alt="Anillo de alas plateado">
-                </div>
-                <div class="carousel-item ">
+            <div class="carousel-item">
+                <div class="d-flex flex-column align-items-center justify-content-center" style="height: 500px; background-color: #e9ecef;">
                     <img src="./assets/imagenes/anillo2.png" class=" w-100" alt="Anillo de corazon gema">
+                    <p style="font-weight:bolder;font-size:larger;color:#e9ecef;position: absolute; top: 10%; left: 22%; transform: translate(-50%, -50%); z-index:10">Descubre nuestras nuevas joyas!</p>
+                    <button class="btn btn-outline-dark" style="position: absolute; top: 20%; left: 16%; transform: translate(-50%, -50%);">Ver productos</button>
                 </div>
-                <div class="carousel-item">
+            </div>
+            <div class="carousel-item">
+                <div class="d-flex flex-column align-items-center justify-content-center" style="height: 500px; background-color: #dee2e6;">
                     <img src="./assets/imagenes/anillo3.png" class=" w-100" alt="Anillo de ala de angel">
+                    <button class="btn btn-outline-dark" style="position: absolute; top: 20%; left: 50%; transform: translate(-50%, -50%); z-index:-1">Ver productos</button>
                 </div>
             </div>
         </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-</div>
-    <div id="info"><p>¡Despachamos todos los días hábiles para que reciba el producto cuanto antes!</p></div>
+
+         <!-- Controls -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
      </section>
+     <div id="info"><p>¡Despachamos todos los días hábiles para que reciba el producto cuanto antes!</p></div>
 <?php include_once("./estructura/footer.php"); ?>
 </body>
 
