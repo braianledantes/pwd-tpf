1a13232d19ac9d11e651ac512cd8fbe34ae0824a
Braian Ledantes <braianledantes@gmail.com>
2024-11-20 20:07:48 -0300

    utiliza variables de entorno

--- configuracion.php
+++ configuracion.php
@@ -9,3 +9,10 @@ $GLOBALS['ROOT'] = $_SERVER['DOCUMENT_ROOT'] . $PROJECT_PATH . "/";
 $GLOBALS['INICIO'] = $PROJECT_PATH . '/vista';
 $GLOBALS['HOST'] = 'http://localhost' . $PROJECT_PATH . '/';
 include_once('util/funciones.php');
+
+// trae las librerias de composer
+require "vendor/autoload.php";
+
+// Carga las variables de entorno
+$dotenv = Dotenv\Dotenv::createImmutable($GLOBALS['ROOT']);
+$dotenv->load();
